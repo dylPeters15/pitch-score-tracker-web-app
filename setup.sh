ef680aefcdc733ec2d06d0074dfb417684cc6464
Dylan Peters <dylanlpeters@gmail.com>
2019-05-23 14:46:10 -0700

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ng new pitch-score-tracker-web-app --routing=true --style=css
+cd pitch-score-tracker-web-app/
+git remote add origin git@github.com:dylPeters15/pitch-score-tracker-web-app.git
+git push -u origin master
